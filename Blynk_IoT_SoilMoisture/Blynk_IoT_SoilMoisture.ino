18494feb0fa08256b8db4b98f2636b076c52b45d
Muhammad Arif Ridhwan Bin Mohamed Jalil <74816249+arifroboshoptech@users.noreply.github.com>
2023-10-13 20:41:46 +0800

    Update Blynk_IoT_SoilMoisture.ino
    updates code

--- Blynk_IoT_SoilMoisture/Blynk_IoT_SoilMoisture.ino
+++ Blynk_IoT_SoilMoisture/Blynk_IoT_SoilMoisture.ino
@@ -1,7 +1,7 @@
 // Fill-in information from your Blynk Template here
-#define BLYNK_TEMPLATE_ID "TMPL6clTUsFEW"
-#define BLYNK_TEMPLATE_NAME "Gas Monitoring"
-#define BLYNK_AUTH_TOKEN "d1jLp_1Kt1bXHMgezZIwnCX7Y9EGTqAp"
+#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME         "Device"
+#define BLYNK_AUTH_TOKEN            "YourAuthToken"
 
 #include <BlynkSimpleEsp32.h>
 
@@ -9,48 +9,36 @@
 #define wifi_password "your wifi password"
 
 //pinout declaratiorn
-#define analogsensorpin 32 //change to where analog sensor is connected
-#define led_red 23
-#define led_green 22
+#define SENSORPIN 35 //soil moisture sensor
+#define RELAY 15 //waterpump
 
-#define treshold 500 //change the treshold value according to your sensor
+#define VIRTUALPIN_RELAY    V1 
 
+BlynkTimer timer;
+
+
+BLYNK_WRITE(VIRTUALPIN_RELAY) {
+  int RelayState = param.asInt();
+  digitalWrite(RELAY,RelayState);
+}
+
+void sendSensor()
+{
+  int sensor_reading = analogRead(SENSORPIN);
+  int map_reading = map(sensor_reading,1800,4095,100,0);
+
+  Blynk.virtualWrite(V0, map_reading);
+}
 
 void setup()
 {
   Serial.begin(115200);
-  delay(100);
-  pinMode(led_red,OUTPUT);
-  pinMode(led_green,OUTPUT);
+  pinMode(RELAY,OUTPUT);
   Blynk.begin(BLYNK_AUTH_TOKEN, wifi_username, wifi_password);
+  timer.setInterval(1000L, sendSensor);
 }
 
 void loop() {
   Blynk.run();
-
-  int analogvalue = analogRead(analogsensorpin);
-
-  Blynk.virtualWrite(V1,analogvalue);
-
-  //print value to Serial Monitor
-
-  Serial.print("Gas Value : "); //change this to what you measure
-  Serial.println(analogvalue);
-
-
-  //led indicator process at the edge
-  
-  //chage the comparator on what are you measure
-  if(analogvalue < treshold){
-    digitalWrite(led_red,HIGH);
-    digitalWrite(led_green,LOW);  
-  }
-  else
-  {
-    digitalWrite(led_red,LOW);
-    digitalWrite(led_green,HIGH);
-    
-  }
-
-  delay(1000);
+  timer.run();
 }
